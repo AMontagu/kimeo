647f46d67658f594702fe85ac7ef6a0465ca1480
AMontagu <adrienmontagu@gmail.com>
2016-03-29 00:19:38 +0200

    first try control motor

--- KimeoApp/RobotCommunication.py
+++ KimeoApp/RobotCommunication.py
@@ -1,12 +1,15 @@
 #This is a singleton class for send action to the robot.
 from django.utils.six import BytesIO
 from rest_framework.parsers import JSONParser
+import pypot.dynamixel
+import time
 
 class RobotCommunication:
     class __RobotCommunication:
         def __init__(self):
             #here put all the variable of the electronical component then create function for each action
             self.serialCom = "tty/com"  # need to be replace with good one
+            self.dxl_io = pypot.dynamixel.DxlIO('/dev/ttyACM0')
 
         def __str__(self):
             return repr(self) + self.serialCom
@@ -27,8 +30,10 @@ class RobotCommunication:
     def move(self, dataSerialized):
         typeMovement = dataSerialized.data['direction'] #data['direction'], data['speedright'], data['speedLeft'], data['duration']
         if typeMovement == "forward":
+            self.dxl_io.set_moving_speed({11: float(dataSerialized.data['speedright'])})
+            time.sleep(float(dataSerialized.data['duration']))
+            self.dxl_io.set_moving_speed({11: 0.0})
             #TODO call serial communication for move forward the robot with speed and duration apropriate
-            pass
         if typeMovement == "backward":
             pass
         if typeMovement == "turnLeft":
