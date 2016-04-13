7aadb6b9ddd8471b5c2bba0d1faf91877c81f074
AMontagu <adrienmontagu@gmail.com>
2016-04-13 15:46:07 +0200

    test other end

--- SerialCommunication/SerialCom.py
+++ SerialCommunication/SerialCom.py
@@ -108,8 +108,8 @@ if __name__ == '__main__':
     while True:
         time.sleep(1)
         print(serialCom.serial_ports())
-        serialCom.write("ligthOn" + "\r\n")
+        serialCom.write("ligthOn" + "\n")
         time.sleep(3)
-        serialCom.write("ligthOff" + "\r\n")
+        serialCom.write("ligthOff" + "\n")
         time.sleep(3)
         #serialCom.write("testtestest" + "\r\n")
