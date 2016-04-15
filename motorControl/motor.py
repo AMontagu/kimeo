3247ec2ad1f07ebbe4ce953ec7b9101e7ad3222c
Ludovic DELVAL <ludovic.delval.external@airbus.com>
2016-04-15 18:37:19 +0200

    Updated Motor

--- motorControl/motor.py
+++ motorControl/motor.py
@@ -12,8 +12,6 @@ class Motor:
             self.available = True
             self.dxl_io = pypot.dynamixel.DxlIO(self.ports[0])
             if(len(self.dxl_io.scan())== 0):
-               self.dxl_io = pypot.dynamixel.DxlIO(self.ports[1])
-               if(len(self.dxl_io.scan())== 0):
                  print('No motor detected (et c est la merde)!')
             self.motorRight = motorRight
             self.motorRightAvailable = False
