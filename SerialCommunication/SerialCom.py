ca3bdc5e052e1f76994f780249ef51ffa78735be
AMontagu <adrienmontagu@gmail.com>
2016-04-23 18:10:34 +0200

    add print precision

--- SerialCommunication/SerialCom.py
+++ SerialCommunication/SerialCom.py
@@ -63,13 +63,13 @@ class SerialCom(threading.Thread):
                                 print("knock r n received from arduino")
                                 playSound("sensor/sensor1.ogg")
                             elif (input == b"knock"):
-                                print("knock received from arduino")
+                                print("knock received bite from arduino")
                                 playSound("sensor/sensor1.ogg")
                             elif (input == b"knock\\r\\n'"):
-                                print("knock r n double anti slash received from arduino")
+                                print("knock r n bite double anti slash received from arduino")
                                 playSound("sensor/sensor1.ogg")
                             elif (input == b"knock\r\n"):
-                                print("knock r n received from arduino")
+                                print("knock r n bite received from arduino")
                                 playSound("sensor/sensor1.ogg")
                         time.sleep(1)
             except IOError:
